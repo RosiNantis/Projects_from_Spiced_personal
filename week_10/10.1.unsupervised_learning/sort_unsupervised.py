7ae0efd8aca412ab488072e50f0fa106b4fd5d37
Alexandros Samartzis <77395762+rosi-nantis@users.noreply.github.com>
2022-05-16 12:37:46 +0200

    week10 prepare files

---
+++ week_10/10.1.unsupervised_learning/sort_unsupervised.py
@@ -0,0 +1,30 @@
+"""
+Reorder the elements from the list on the right to complete
+the right sentences beginning in the list on the left.
+"""
+
+left = [
+ 'Outlier or anomaly detection is used',
+ 'Principal Component Analysis (PCA) is a method',
+ 'Principal Component Analysis (PCA) is frequently used',
+ 'There are many clustering methods',
+ 'Non-negative Matrix Factorization (NMF) is an algorithm',
+ 'Most clustering algorithms are based on a distance metric',
+ 'Gaussian Mixture Models (GMM) are a generative model',
+ 'Unsupervised learning is a family of machine learning methods',
+ 'The "curse of dimensionality" is a problem',
+ 't-SNE reduces data to two dimensions'
+]
+
+right = [
+ 'frequently used in recommender systems and customer segmentation.',
+ 'to visualize complex datasets.',
+ 'that becomes worse the more features you have.',
+ 'to identify credit card fraud.',
+ 'for dimensionality reduction.',
+ 'e.g. Euclidean or Manhatten distance.',
+ 'that do not require labled data.',
+ 'for detecting outliers.',
+ 'as part of a supervised learning pipeline.',
+ 'like K-means, DBSCAN or Ward.'
+]
