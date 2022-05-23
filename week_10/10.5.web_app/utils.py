9267f3f718632b3029710524d769ad077dbcc069
Alexandros Samartzis <77395762+rosi-nantis@users.noreply.github.com>
2022-05-24 01:48:15 +0200

    debugg utilis file

--- week_10/10.5.web_app/utils.py
+++ week_10/10.5.web_app/utils.py
@@ -21,32 +21,45 @@ mtg = pd.read_csv('../data/ml-latest-small/movies.csv', na_values = 'Nan')
 # merge the two frames based on the column movieid
 movies = pd.merge(umrT, mtg, on='movieId')
 
-
-
-
 methods_recommendation = ['random','NMF','user_similarity']
 
 # load model
 with open('static/model5.pkl', 'rb') as f:
     model= pickle.load(f)
 
-def get_movie_frame(umrT=umrT, mtg = mtg):
-    """
-    i will get a Data Frame with movieId Title average ratings for per user.
-    Final outcome is a frame with columns:# 
-    movieID, userID, avg(rate), title, genres, accurate(rate)
-    """
-    umrT_av_rat = umrT.set_index('movieId').groupby(['movieId']).mean()
-    # merge the two frames based on the column movieid
-    movies = pd.merge(umrT, mtg, on='movieId')
-    # merge the two frames based on the column movieid to find average rating
-    movieId_rating = pd.merge(umrT_av_rat, mtg, on='movieId')
-    # merge movieID, userID, avg(rate), title, genres, accurate(rate)
-    mov = pd.merge(movieId_rating,movies, on='movieId', how = 'left')[["movieId", "rating_x","title_x","userId_y","genres_x","rating_y"]]
-    mov.columns = [["movieId", "rating_avg","title","userId","genres","rating_acc"]]
-    mov.head(5)
-    # reset index
-    movies = mov
+
+def get_movie_frame(method = methods_recommendation, umrT=umrT, mtg = mtg):
+
+    if method == 'NMF':
+        """
+        i will get a Data Frame with movieId Title and userId 
+        pivoted in a matrix with NaN where user has no rating
+        """
+        # use pivot to make the matrix of movie rates
+        rates =umrT.pivot(index='userId',columns = 'movieId')
+        # Split the movie name from movie year and apply it in the matrix
+        mtg['year'] = mtg.title.astype(str).str[-6:]
+        mtg['title_new'] = mtg.title.astype(str).str[:-6]
+        # Try to zip columns with movie names
+        #new_columns = dict(zip(df.movieId,df_movie.title))
+        rates.rename(columns=dict(zip(mtg["movieId"], mtg["title_new"])),inplace = True)
+        movies = rates.rating
+    elif method == 'user_similarity':
+        """
+        i will get a Data Frame with movieId Title average ratings for per user.
+        Final outcome is a frame with columns:# 
+        movieID, userID, avg(rate), title, genres, accurate(rate)
+        """
+        umrT_av_rat = umrT.set_index('movieId').groupby(['movieId']).mean()
+        # merge the two frames based on the column movieid
+        movies = pd.merge(umrT, mtg, on='movieId')
+        # merge the two frames based on the column movieid to find average rating
+        movieId_rating = pd.merge(umrT_av_rat, mtg, on='movieId')
+        # merge movieID, userID, avg(rate), title, genres, accurate(rate)
+        mov = pd.merge(movieId_rating,movies, on='movieId', how = 'left')[["movieId", "rating_x","title_x","userId_y","genres_x","rating_y"]]
+        mov.columns = [["movieId", "rating_avg","title","userId","genres","rating_acc"]]
+        # reset index
+        movies = mov
     return movies
 
 def match_movie_title(input_title, movie_titles):
